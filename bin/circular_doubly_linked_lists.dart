950c589411bebc3a6ced21a7fed117b5be2b3834
moein-keyvani-pur <moein.kpr@gmail.com>
2022-08-26 19:18:15 +0430

    change some file

--- bin/circular_doubly_linked_lists.dart
+++ bin/circular_doubly_linked_lists.dart
@@ -2,5 +2,5 @@ import 'package:circular_doubly_linked_lists/circular_doubly_linked_lists.dart'
     as circular_doubly_linked_lists;
 
 void main(List<String> arguments) {
-  // print('Hello world: ${circular_doubly_linked_lists.calculate()}!');
+  print('Hello world: ${circular_doubly_linked_lists.calculate()}!');
 }
